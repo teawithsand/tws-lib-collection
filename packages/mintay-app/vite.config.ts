6936e2df509b94e3f9d9ea2d23b22ed4fc3bc58e
teawithsand <teawithsand@gmail.com>
2025-06-15 16:41:11 +0000

    build(mintay-app): :wrench: Added @tabler/icons-react alias to use esm, so that tree shaking works and vite loads faster

--- packages/mintay-app/vite.config.ts
+++ packages/mintay-app/vite.config.ts
@@ -5,6 +5,11 @@ import path from "node:path";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			'@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+		}
+	},
 	css: {
 		preprocessorOptions: {
 			scss: {
