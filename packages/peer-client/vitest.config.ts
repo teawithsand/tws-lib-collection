7eb05dc0b9c96edc57d04c051b9ffb936a24f891
teawithsand <teawithsand@gmail.com>
2025-06-21 14:47:52 +0000

    build(peer-client): :wrench: Refactored vitest configuration to use newer syntax

--- packages/peer-client/vitest.config.ts
+++ packages/peer-client/vitest.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
-	
 	test: {
 		globals: true,
 		environment: "jsdom",
@@ -11,5 +10,30 @@ export default defineConfig({
 			provider: "istanbul",
 		},
 		watch: false,
+		projects: [
+			{
+				test: {
+					include: [
+						"src/**/*.spec.ts",
+					],
+					browser: {
+						enabled: true,
+						provider: "playwright",
+						instances: [
+							{ browser: "firefox", isolate: true, headless: true },
+							{ browser: "chromium", isolate: true, headless: true },
+							{ browser: "webkit", isolate: true, headless: true },
+						]
+					},
+				},
+			},
+			{
+				test: {
+					include: [
+						"src/**/*.test.ts",
+					],
+				},
+			},
+		],
 	},
 })
