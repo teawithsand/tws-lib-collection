527913a5a24b3d58a428f754a7929c3dbaa78231
teawithsand <teawithsand@gmail.com>
2025-07-09 16:57:25 +0000

    feat(reserd): :sparkles: Added SerializerUnknown interface, which allows deserializing from unknown, but serializing to known type.

--- packages/reserd/src/serialization/serializer.ts
+++ packages/reserd/src/serialization/serializer.ts
@@ -23,6 +23,35 @@ export interface Serializer<Stored, Owned> {
 
 /**
  * Interface for serializing and deserializing between Owned and Stored types
+ * @template Owned The owned data type (application format)
+ * @template Stored The stored data type (serialized format)
+ *
+ * This interface allows for serializing to known type, but deserializing from unknown type, which is often
+ * the case when dealing with external data sources or APIs where the exact type of stored
+ * data may not be known or trusted.
+ *
+ * This interface is not deprecated, since it already has proper generic argument order.
+ */
+export interface SerializerUnknown<Owned, Stored>
+	extends SerializerReverse<Owned, Stored> {
+	/**
+	 * Converts an owned object to its stored representation
+	 * @param owned Object in application format
+	 * @returns Object in serialized format
+	 */
+	serialize: (owned: Owned) => Stored
+
+	/**
+	 * Converts a stored representation back to an owned object from unknown input
+	 * @param stored Object in unknown format that should be deserializable to application format
+	 * @returns Object in application format
+	 * @throws May throw if the stored data cannot be deserialized to the expected format
+	 */
+	deserialize: (stored: unknown) => Owned
+}
+
+/**
+ * Interface for serializing and deserializing between Owned and Stored types
  * @template Stored The stored data type (serialized format)
  * @template Owned The owned data type (application format)
  *
